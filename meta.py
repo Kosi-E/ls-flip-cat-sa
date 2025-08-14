4e8a13f3d1c44834926282f1c6cac611f0cd21ae
KOSISOCHUKWU ETIMBUK-UDOEKONG <kosieduzor@gmail.com>
2025-08-15 00:58:37 +0100

    read the dataset, carried out  basic EDA, preprosessing the text data, removed special characters and numbers, tokenize the text, removed stopwords, and carried out lematization

--- meta.py
+++ meta.py
@@ -19,4 +19,32 @@ dd = plt.style.use('ggplot')
 
 ## Reading the dataset
 df = pd.read_csv("C:/Users/Asus/OneDrive/Documents/Learning Saint/Task 1/archive (2)/Dataset-SA.csv")
+print(df.shape)
+print(df.head(100))
 
+## EDA
+ax = df['Rate'].value_counts().sort_index().plot(kind='bar',title='Count Of Reviews by Rates', figsize=(10, 5), color=['#FF5733'])
+ax.set_xlabel('Rate')
+ax.set_ylabel('Count of Reviews')
+plt.show()
+
+
+## Preprocessing the text data
+def preprocess_review(text):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-z\s]', '', text)
+    
+    # Tokenize the text
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    
+    return ' '.join(tokens)
